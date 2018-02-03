69e3ad6c350a2e351c094186bd4a0c23249ee31a
jrwarwick <justin.warwick@gmail.com>
2018-02-02 16:17:53 -0800

    proper scoping via self for the jira interface object.

--- __init__.py
+++ __init__.py
@@ -63,7 +63,7 @@ class JIRASkill(MycroftSkill):
             LOGGER.error(e)
         try:
             #(fallback?)#jira = JIRA(server=os.environ['JIRA_SERVER_URL'],basic_auth=(os.environ['JIRA_USER'],os.environ['JIRA_PASSWORD'])) #  http://bakjira01.int.bry.com:8080/rest/api/2/        
-            jira = JIRA(server=self.settings.get("url", ""),basic_auth=(self.settings.get("username", ""),self.settings.get("password", "")) )
+            self.jira = JIRA(server=self.settings.get("url", ""),basic_auth=(self.settings.get("username", ""),self.settings.get("password", "")) )
             #  http://bakjira01.int.bry.com:8080/rest/api/2/
         except Exception as e:
             LOGGER.error(e)
@@ -95,20 +95,20 @@ class JIRASkill(MycroftSkill):
     # the method is called.
     def handle_status_report_intent(self, message):
         self.speak("JIRA Service Desk status report:")
-        inquiry = jira.search_issues('assignee is EMPTY AND status != Resolved ORDER BY createdDate DESC')
+        inquiry = self.jira.search_issues('assignee is EMPTY AND status != Resolved ORDER BY createdDate DESC')
         if inquiry.total < 1:
             self.speak( "No JIRA issues found in the unassigned queue." )
         else:
             self.speak( str( inquiry.total ) + " issues found in the unassigned queue." )
-            thissue = jira.issue(inquiry[0].key,fields='summary,comment')
+            thissue = self.jira.issue(inquiry[0].key,fields='summary,comment')
             self.speak( "Latest issue is regarding: " + re.sub('([fF][wW]:)+','',thissue.fields.summary) )
 
-        inquiry = jira.search_issues('resolution = Unresolved AND priority > Medium ORDER BY priority DESC')
+        inquiry = self.jira.search_issues('resolution = Unresolved AND priority > Medium ORDER BY priority DESC')
         if inquiry.total < 1:
             self.speak( "No HIGH priority JIRA issues remain open." )
         else:
             self.speak( str( inquiry.total ) + " high priority issue" + ('','s')[inquiry.total > 1] + " remain" + ('s','')[inquiry.total > 1] + " open!" )
-            thissue = jira.issue(inquiry[0].key,fields='summary,comment')
+            thissue = self.jira.issue(inquiry[0].key,fields='summary,comment')
             self.speak( "Highest priority issue is regarding: " + re.sub('([fF][wW]:)+','',thissue.fields.summary) )
 
 
